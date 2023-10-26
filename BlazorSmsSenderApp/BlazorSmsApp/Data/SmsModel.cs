714192e7ff870e964de95cde1674cd39d8b71153
nicolasmahlang75 <nicholus.mahlangu@apex-rts.co.za>
2023-10-26 17:08:20 +0200

    Initail commit - the app is working perfectly fine, but the sms doesn't go through

---
+++ BlazorSmsSenderApp/BlazorSmsApp/Data/SmsModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace BlazorSmsApp.Data
+{
+    public class SmsModel
+    {
+        public string RecipientNumber { get; set; }
+        public string TextMessage { get; set; }
+    }
+}
---
+++ BlazorSmsSenderApp/BlazorSmsApp/Data/SmsService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Vonage.Messaging;
+using Vonage.Request;
+
+namespace BlazorSmsApp.Data
+{
+	public class SmsService
+	{
+		public IConfiguration Configuration { get; set; }	
+		public SmsService(IConfiguration config) {  Configuration = config; }
+
+        public SendSmsResponse SendSms(SmsModel smsObj)
+        {
+			var credentials = Credentials.FromApiKeyAndSecret("e5b4b7e9","cW8zWzthpClxf19Z");
+			//var VonageClient = new VonageClient(credentials);
+			//var creds = Credentials.FromApiKeyAndSecret("e5b4b7e9", "cW8zWzthpClxf19Z");
+            var client = new SmsClient(credentials);
+            
+			var response = new SendSmsRequest()
+			{
+				To = smsObj.RecipientNumber,
+				From = "Nicholus Mahlangu",
+				Text = smsObj.TextMessage
+			};
+
+			return client.SendAnSms(response);
+        }
+    }
+
+	
+}
